432473cf5d644b1a040c0372bfc606f30a148150
davihdsantos <davihenriqueds@gmail.com>
2023-03-06 10:25:55 -0300

    cleaned vsnt_teleop node

--- src/vsnt_teleop.cpp
+++ src/vsnt_teleop.cpp
@@ -1,37 +1,9 @@
-//=================================================================================================
-// Copyright (c) 2012-2016, Institute of Flight Systems and Automatic Control,
-// Technische Universität Darmstadt.
-// All rights reserved.
-
-// Redistribution and use in source and binary forms, with or without
-// modification, are permitted provided that the following conditions are met:
-//     * Redistributions of source code must retain the above copyright
-//       notice, this list of conditions and the following disclaimer.
-//     * Redistributions in binary form must reproduce the above copyright
-//       notice, this list of conditions and the following disclaimer in the
-//       documentation and/or other materials provided with the distribution.
-//     * Neither the name of hector_quadrotor nor the names of its contributors
-//       may be used to endorse or promote products derived from this software
-//       without specific prior written permission.
-
-// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
-// ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
-// WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-// DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER BE LIABLE FOR ANY
-// DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
-// (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-// LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
-// ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
-// SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-//=================================================================================================
-
 #include <ros/ros.h>
 #include <sensor_msgs/Joy.h>
 #include <mavros_msgs/OverrideRCIn.h>
 
 
-namespace usv_sim
+namespace vsnt_teleop
 {
 
 class Teleop
@@ -129,13 +101,7 @@ public:
     double rudder, sail;
 
     joint_cmd.channels[0] = (getAxis(joy, axes_.yaw));
-    // joint_cmd.channels[1] = (getAxis(joy, axes_.z));
     joint_cmd.channels[2] = (getAxis(joy, axes_.thrust));
-    // joint_cmd.channels[3] = (getAxis(joy, axes_.z));
-    // joint_cmd.channels[4] = (getAxis(joy, axes_.z));
-    // joint_cmd.channels[5] = (getAxis(joy, axes_.z));
-    // joint_cmd.channels[6] = (getAxis(joy, axes_.z));
-    // joint_cmd.channels[7] = (getAxis(joy, axes_.z));
 
     joint_publisher_.publish(joint_cmd);
   }
@@ -173,13 +139,13 @@ public:
   }
 };
 
-} // namespace hector_quadrotor
+}
 
 int main(int argc, char **argv)
 {
-  ros::init(argc, argv, "usv_teleop");
+  ros::init(argc, argv, "vsnt_teleop");
 
-  usv_sim::Teleop teleop;
+  vsnt_teleop::Teleop teleop;
   ros::spin();
 
   return 0;
